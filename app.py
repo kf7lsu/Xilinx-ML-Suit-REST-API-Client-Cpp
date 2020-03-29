aa1ec16d6f50bbf421a02ffeb52aefd73e6f24eb
Tom Lou <louyu27@cs.washington.edu>
2020-03-29 13:11:34 -0700

    Print timing profile

--- app.py
+++ app.py
@@ -1,102 +1,141 @@
 from __future__ import print_function
 
-import os,sys,argparse
+import argparse
+import json
+import time
 
 import caffe
 import flask
-import io
-import json
 import numpy as np
+import xdnn_io
 
 app = flask.Flask(__name__)
- 
-def LoadImage(prototxt,caffemodel,labels):
-  import numpy as np
-  import xdnn_io
-  global net
-  net = caffe.Net(prototxt,caffemodel,caffe.TEST)
-  print (net.blobs['data'].data.shape)
-  return net  
 
-def InferImage(net,image,labels):
-  import numpy as np
-  import xdnn_io
-  transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
-  transformer.set_transpose('data', (2,0,1))
-  # transformer.set_mean('data', np.array([104,117,123]))
-  # transformer.set_raw_scale('data', 255)
-  transformer.set_channel_swap('data', (2,1,0)) # if using RGB instead if BGR
-  net.blobs['data'].data[...] = transformer.preprocess('data', image)
-  ptxtShape = net.blobs["data"].data.shape
-  print ("Running with shape of: ",ptxtShape)
-  out = net.forward()
-  for key in out:
-    try:
-      if out[key].shape[1] == 1000:
-        softmax = out[key]
-    except:
-      pass
-  Labels = xdnn_io.get_labels(labels)
-  xdnn_io.printClassification(softmax,[image],Labels)
-  return xdnn_io.getClassification(softmax,[image],Labels) 
+net = None
+tranformer = None
+
+
+class Job:
+    def __init__(self):
+        self.img = None
+        self.profile = {}
+
+    def start_timer(self):
+        self.profile["start_time"] = time.time()
+
+    def record_time(self, name):
+        self.profile[name] = time.time() - self.profile["start_time"]
+
+    def print_profile(self):
+        records = [(key, value) for (key, value) in self.profile]
+        records.sort(key=lambda x: x[1])
+        for key, value in records:
+            print(key, ":", value)
+
+
+def LoadImage(prototxt, caffemodel, labels):
+    global net
+    global transformer
+    net = caffe.Net(prototxt, caffemodel, caffe.TEST)
+    print(net.blobs['data'].data.shape)
+
+    # Setup preprocessor
+    transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
+    transformer.set_transpose('data', (2, 0, 1))
+    # transformer.set_mean('data', np.array([104,117,123]))
+    # transformer.set_raw_scale('data', 255)
+    transformer.set_channel_swap('data', (2, 1, 0))  # if using RGB instead if BGR
+
+
+def InferImage(net, image, labels):
+    global transformer
+    net.blobs['data'].data[...] = transformer.preprocess('data', image)
+    out = net.forward()
+    for key in out:
+        try:
+            if out[key].shape[1] == 1000:
+                softmax = out[key]
+        except:
+            pass
+    Labels = xdnn_io.get_labels(labels)
+    xdnn_io.printClassification(softmax, [image], Labels)
+    return xdnn_io.getClassification(softmax, [image], Labels)
+
 
 @app.route("/predict", methods=["POST"])
 def predict():
-  data = {"success": False}
+    job = Job()
+    job.start_timer()
 
-  global prototxt
-  global model
-  global synset_words
+    data = {"success": False}
 
-  if flask.request.method == "POST":
-    # Get image
-    images = flask.request.form["image"]
+    global prototxt
+    global model
+    global synset_words
 
-    # Decode array
-    images = json.loads(images)
+    if flask.request.method == "POST":
+        # Get image
+        images = flask.request.form["image"]
 
-    # Inference
-    images = np.array(images, dtype=np.float32)
-    images = images.reshape((-1, 224, 224, 3))
+        # Decode array
+        images = json.loads(images)
 
-    responses = []
+        job.record_time("decode_image")
 
-    for i in range(images.shape[0]):
-      image = images[i]
-      responses.append(InferImage(net, image, synset_words))
+        # Inference
+        images = np.array(images, dtype=np.float32)
+        images = images.reshape((-1, 224, 224, 3))
 
-    # Write response
-    data["success"] = True
-    data["response"] = responses
+        job.record_time("convert_to_np_array")
 
-  return flask.jsonify(data)
+        responses = []
+
+        for i in range(images.shape[0]):
+            image = images[i]
+            responses.append(InferImage(net, image, synset_words))
+
+        job.record_time("inference")
+
+        # Write response
+        data["success"] = True
+        data["response"] = responses
+
+        job.record_time("save_result")
+
+    result = flask.jsonify(data)
+
+    job.record_time("encode_result")
+    job.print_profile()
+
+    return result
+
 
 if __name__ == "__main__":
-  parser = argparse.ArgumentParser(description='pyXFDNN')
-  parser.add_argument('--caffemodel', default="/opt/models/caffe/bvlc_googlenet/bvlc_googlenet.caffemodel", help='path to caffemodel file eg: /opt/models/caffe/bvlc_googlenet/bvlc_googlenet.caffemodel')
-  parser.add_argument('--prototxt', default="xfdnn_auto_cut_deploy.prototxt", help='path to  prototxt file eg: xfdnn_auto_cut_deploy.prototxt')
-  parser.add_argument('--synset_words', default="$HOME/CK-TOOLS/dataset-imagenet-ilsvrc2012-aux/synset_words.txt", help='path to synset_words eg: $HOME/CK-TOOLS/dataset-imagenet-ilsvrc2012-aux/synset_words.txt')
-  parser.add_argument('--port', default=5000)
-  
-  args = vars(parser.parse_args())
+    parser = argparse.ArgumentParser(description='pyXFDNN')
+    parser.add_argument('--caffemodel', default="/opt/models/caffe/bvlc_googlenet/bvlc_googlenet.caffemodel",
+                        help='path to caffemodel file eg: /opt/models/caffe/bvlc_googlenet/bvlc_googlenet.caffemodel')
+    parser.add_argument('--prototxt', default="xfdnn_auto_cut_deploy.prototxt",
+                        help='path to  prototxt file eg: xfdnn_auto_cut_deploy.prototxt')
+    parser.add_argument('--synset_words', default="$HOME/CK-TOOLS/dataset-imagenet-ilsvrc2012-aux/synset_words.txt",
+                        help='path to synset_words eg: $HOME/CK-TOOLS/dataset-imagenet-ilsvrc2012-aux/synset_words.txt')
+    parser.add_argument('--port', default=5000)
 
-  if args["caffemodel"]:
-    model=args["caffemodel"]
-  
-  if args["prototxt"]:
-    prototxt=args["prototxt"]
+    args = vars(parser.parse_args())
 
-  if args["synset_words"]:
-    synset_words=args["synset_words"]
-  
-  if args["port"]:
-    port=args["port"]
-  else:
-    port=9000 
+    if args["caffemodel"]:
+        model = args["caffemodel"]
 
-  print("Loading FPGA with image...")
-  net = LoadImage(prototxt, model, synset_words)
-  
-  print("Starting Flask Server...")
-  app.run('0.0.0.0', port=port)
+    if args["prototxt"]:
+        prototxt = args["prototxt"]
 
+    if args["synset_words"]:
+        synset_words = args["synset_words"]
+
+    if args["port"]:
+        port = args["port"]
+
+    print("Loading FPGA with image...")
+    LoadImage(prototxt, model, synset_words)
+
+    print("Starting Flask Server...")
+    app.run('0.0.0.0', port=port)
